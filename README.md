a2b9d170d3a5e4bb61b6bc49a6765d071c365026
Maciej Majewski <maciej.majewski@gooddesign.pl>
2023-02-10 19:33:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LightBox Ingredients highlighter - Chrome Plugin
+
+Simple plugin for highlighting ingredients in recipes.
+
+This is PoC and only highlights following ingredients:
+* bell pepper (pl: "papryka")
+* turkey (pl: "indyk")
+
+## Installation
+
+1. Enable developer mode in Chrome
+2. Load unpacked extension
---
+++ src/ingredients.js
@@ -0,0 +1,81 @@
+const ingredients = {
+  papryka: ["papryk"],
+  indyk: ["indyk", "indycz"],
+}
+
+const container = document.querySelector("#list-of-days-to-change");
+
+function createInfoBox() {
+  const header = document.createElement("div");
+  header.textContent = "Przeanalizowano menu w poszukiwaniu składników: " + Object.keys(ingredients).join(", ");
+  header.classList.add("chrome-plugin-ingredients-custom-box");
+  header.classList.add("info");
+  return header;
+}
+function createIngredientBox(names) {
+  const header = document.createElement("div");
+  header.textContent = names.join(", ");
+  header.classList.add("chrome-plugin-ingredients-custom-box");
+  header.classList.add("ingredient");
+  return header;
+}
+
+function anyMatch(source, matchWords) {
+  for (const word of matchWords) {
+    if (source.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function highlightIngredients() {
+  const containers = document.querySelectorAll('.flexch-dish');
+
+  for (const dishContainer of containers) {
+    const activeDishContainer = dishContainer.querySelector('.flexch-dish__meals__meal.active');
+    const dishIns = activeDishContainer.querySelectorAll('.dish-in');
+
+    for (const dishInElement of dishIns) {
+      const dishIngredients = dishInElement.dataset.in;
+
+      const matchedIngredients = [];
+
+      for (const [ingredientName, matchWords] of Object.entries(ingredients)) {
+        if (anyMatch(dishIngredients, matchWords)) {
+          matchedIngredients.push(ingredientName);
+        }
+      }
+
+      if (matchedIngredients.length > 0) {
+        dishContainer.classList.add("chrome-plugin-ingredients-custom-class");
+        dishContainer.classList.add("alert");
+        dishContainer.insertAdjacentElement("beforeend", createIngredientBox(matchedIngredients));
+      }
+    }
+  }
+}
+
+function resetIngredients() {
+  for (const element of document.querySelectorAll('.chrome-plugin-custom-css')) {
+    element.classList.remove("chrome-plugin-custom-css");
+  }
+  
+  highlightIngredients();
+}
+
+function main() {
+  container.insertAdjacentElement("afterbegin", createInfoBox());
+  highlightIngredients();
+
+  for (const submitButton of document.querySelectorAll(".flexch-dish__meals__meal__name")) {
+    if (submitButton.innerText === "Wybierz") {
+      console.log(submitButton);
+    }
+  }
+}
+
+if (container) {
+  main();
+}
